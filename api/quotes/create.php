02333cb5d43c11cfec8d168e8c4938a2d55d654e
vincentmays006 <104940949+vincentmays006@users.noreply.github.com>
2024-03-14 12:13:08 -0600

    trying to get port to work

--- api/quotes/create.php
+++ api/quotes/create.php
@@ -40,7 +40,7 @@
             //Create post
             if($quote->create()) {
                 $quote_item = array(
-                    'id' => $data->id,
+                    'id' => $quote->id,
                     'quote' => $data->quote,
                     'author_id' => $data->author_id,
                     'category_id' => $data->category_id,
