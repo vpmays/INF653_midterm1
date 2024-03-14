50266a765c3c3571f788b511126f914937d64724
vincentmays006 <104940949+vincentmays006@users.noreply.github.com>
2024-03-14 15:46:00 -0600

    trying to get port to work

--- api/quotes/create.php
+++ api/quotes/create.php
@@ -32,7 +32,7 @@
             echo json_encode(
                 array('message' => 'category_id Not Found')
             );*/
-        if (!isset($data->author_id) || !isset($data->quote) || !isset($data->category_id)) { //(!isset($data->author_id) || !isset($data->quote) || !isset($data->category_id))
+        if (!isset($data->author_id) || !$data->author_id || !isset($data->quote) || !$data->quote_id || !isset($data->category_id) || !$data->category_id) { //(!isset($data->author_id) || !isset($data->quote) || !isset($data->category_id))
             echo json_encode(
                 array('message' => 'Missing Required Parameters') //'Missing Required Parameters'
             );
