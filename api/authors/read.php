a3e785816526f9c1bbdcf3d430781ebc39f4f64f
vincentmays006 <104940949+vincentmays006@users.noreply.github.com>
2024-03-14 10:11:24 -0600

    trying to get port to work

--- api/authors/read.php
+++ api/authors/read.php
@@ -1,8 +1,8 @@
 <?php
     function read() {
         //Headers
-        header('Access-Control-Allow-Origin: *');
-        header('Content-Type: application/json');
+        //header('Access-Control-Allow-Origin: *');
+        //header('Content-Type: application/json');
 
         include_once '../../config/Database.php';
         //include_once '../../config/Database_local.php';
